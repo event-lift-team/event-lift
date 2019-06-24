af1da8bd062920a857f3f112cdfe79db1421dafe
dawidsau <dawidsau@gmail.com>
2019-06-24 22:08:26 +0200

    Remove not needed imports

--- src/main/java/pl/sda/eventlift/events/controller/EventsController.java
+++ src/main/java/pl/sda/eventlift/events/controller/EventsController.java
@@ -1,30 +1,24 @@
 package pl.sda.eventlift.events.controller;
 
-import javafx.scene.control.Alert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.jsf.FacesContextUtils;
 import pl.sda.eventlift.events.model.Countries;
 import pl.sda.eventlift.events.pojo.EventDTO;
 import pl.sda.eventlift.events.services.EventsService;
 
-import javax.swing.*;
-import java.awt.*;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 
-
 @Controller
-public class EventsController  {
+public class EventsController {
 
     private EventsService eventsService;
 
